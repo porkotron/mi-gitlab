df071143a73269877265223c54f8ea4a74235cd5
porkotron <laskipeto@gmail.com>
2016-03-06 20:48:52 +0200

    Update components and switch database
    -Image base-64-15.4.0
    -gitlab 8.5-stable
    -workhorse 0.6.5
    -gitlab-shell v2.6.10
    -switch database to postgresql (recommended)
    -use ruby 2.1.x (supported)

--- copy/var/zoneinit/includes/31-gitlab.sh
+++ copy/var/zoneinit/includes/31-gitlab.sh
@@ -1,29 +1,19 @@
 
-log "getting mysql_pw"
+log "getting postgresql_pw"
 
-MYSQL_PW=${MYSQL_PW:-$(mdata-get mysql_pw 2>/dev/null)} || \
-MYSQL_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+POSTGRESQL_PW=${POSTGRESQL_PW:-$(mdata-get postgresql_pw 2>/dev/null)} || \
+POSTGRESQL_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
 
-MYSQL_INIT="DELETE from mysql.user;
-GRANT ALL on *.* to 'root'@'localhost' identified by '${MYSQL_PW}' with grant option;
-GRANT ALL on *.* to 'root'@'${PRIVATE_IP:-${PUBLIC_IP}}' identified by '${MYSQL_PW}' with grant option;
-CREATE DATABASE IF NOT EXISTS gitlabhq_production DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'gitlab'@'localhost' IDENTIFIED BY '${MYSQL_PW}';
-GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON gitlabhq_production.* TO 'gitlab'@'localhost';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;"
-
-log "starting the MySQL instance"
-svcadm enable percona
+log "starting the PostgreSQL instance"
+svcadm enable -r svc:/pkgsrc/postgresql:default
 
 log "waiting for the socket to show up"
 COUNT="0";
-while [[ ! -e /tmp/mysql.sock ]]; do
+while [[ ! -e /var/pgsql/data/postmaster.pid ]]; do
         sleep 1
         ((COUNT=COUNT+1))
         if [[ $COUNT -eq 60 ]]; then
-          log "ERROR Could not talk to MySQL after 60 seconds"
+          log "ERROR Could not talk to PostgreSQL after 60 seconds"
     ERROR=yes
     break 1
   fi
@@ -33,12 +23,13 @@ log "(it took ${COUNT} seconds to start properly)"
 
 sleep 1
 
-[[ "$(svcs -Ho state percona)" == "online" ]] || \
-  ( log "ERROR MySQL SMF not reporting as 'online'" && exit 31 )
+[[ "$(svcs -Ho state postgresql)" == "online" ]] || \
+  ( log "ERROR PostgreSQL SMF not reporting as 'online'" && exit 31 )
 
 log "running the access lockdown SQL query"
-mysql -u root -e "${MYSQL_INIT}" >/dev/null || \
-  ( log "ERROR MySQL query failed to execute." && exit 31 )
+
+sudo PGPASSWORD=postgres psql -U postgres -w -d template1 -c "ALTER USER gitlab WITH PASSWORD '${POSTGRESQL_PW}'" >/dev/null || \
+  ( log "ERROR PostgreSQL query failed to execute." && exit 31 ) 
 
 log "configuring redis to bind to localhost only"
 gsed -i \
@@ -84,11 +75,14 @@ GITLAB_ROOT_PW="5iveL!fe";
 log "configuring gitlab"
 cd /home/git/gitlab
 gsed -i \
-        -e "s/%MYSQL_PW%/${MYSQL_PW}/" \
+        -e "s/%POSTGRESQL_PW%/${POSTGRESQL_PW}/" \
         /home/git/gitlab/config/database.yml
 gsed -i \
         -e "s/%HOSTNAME%/${HOSTNAME}/" \
         /home/git/gitlab/config/gitlab.yml
+gsed -i \
+        -e "s/%HOSTNAME%/${HOSTNAME}/" \
+        /opt/local/etc/nginx/nginx.conf
 sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production GITLAB_ROOT_PASSWORD="${GITLAB_ROOT_PW}" force=yes
 
 log "starting the postfix instance"
